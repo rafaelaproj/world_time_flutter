78c172d3b56bf70739019673aafb9962563d73ef
Rafaela Souza <rafaelaproj@gmail.com>
2019-12-22 10:22:34 -0300

    Add files via upload

---
+++ lib/services/world_time.dart
@@ -0,0 +1,46 @@
+import 'package:http/http.dart';
+import 'package:intl/intl.dart';
+import 'dart:convert';
+
+class WorldTime{
+
+  String location;   //location name for the UI
+  String time;       //time in that location
+  String flag;       //url to an asset flag icon
+  String url;        //location url for api endpoint
+  bool isDayTime = false;    //true or false if daytime or not
+
+  WorldTime({ this.location, this.flag, this.url });
+
+  Future<void> getTime() async {
+
+    try{
+      //make the request
+      Response response = await get('http://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body);
+      //print(data);
+
+      //get properties from data
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1,3);
+      //print(datetime);
+      //print(offset);
+
+      //create Datetime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+
+      //set the time property
+      isDayTime = now.hour > 6 && now.hour < 20 ? true : false;  //between 6am and 8pm
+      time = DateFormat.jm().format(now);
+    } catch(e) {
+      print('caught error: $e');
+      time = 'could not get time data';
+    }
+
+
+  }
+
+
+}
+
